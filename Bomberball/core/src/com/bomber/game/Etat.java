b0dcf8019d0dfd31d789029839cb0488554c3525
Paul-Louis <prenard@enssat.fr>
2019-01-05 16:33:06 +0100

    PF

--- Bomberball/core/src/com/bomber/game/Etat.java
+++ Bomberball/core/src/com/bomber/game/Etat.java
@@ -1,20 +1,14 @@
 package com.bomber.game;
 
-import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 
 public abstract class Etat {//classe etat
-    protected Jeu jeu;
+    private Jeu jeu;
     public Etat(Jeu jeu){
         this.jeu=jeu;// on prend le jeu pour un changement d'etat eventuel (à voir si besoin)
     }
-
-
-
     public abstract boolean keyDown(InputEvent event, int keycode);//traitement des inputs
     public abstract boolean touchDown(InputEvent event, float x, float y, int pointer, int button);
-    public abstract boolean mouseMoved(InputEvent event, float x, float y);
-
 
 
 }
--- Bomberball/core/src/com/bomber/game/MurI.java
+++ Bomberball/core/src/com/bomber/game/MurI.java
@@ -4,6 +4,7 @@ public class MurI extends Mur{
 
     public MurI(){
         super(Bomberball.multiTexture[2]);
+        setName("MurI");
     }
 
     public boolean destructible() {
