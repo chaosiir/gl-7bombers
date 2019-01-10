adce3bcfd3f205f8b5cde2411f26fa4eaf1b2d0f
Theo <tlois@enssat.fr>
2019-01-10 23:50:41 +0100

    MaJ multi 1

--- Bomberball/core/src/com/bomber/game/Multijoueur.java
+++ Bomberball/core/src/com/bomber/game/Multijoueur.java
@@ -159,124 +159,123 @@ public class Multijoueur extends Etat implements Screen {//etat multijoueur
         player1=new Image(Bomberball.multiTexture[4]);
         player1.setBounds(3*Bomberball.taillecase+30,Gdx.graphics.getHeight()-Bomberball.taillecase-50,Bomberball.taillecase,Bomberball.taillecase);
 
+
         joueur2=new Image(new Texture(Gdx.files.internal("Panneau_joueur.png")));
         joueur2.setWidth(jeu.map.getX()+2f*Bomberball.taillecase);
-        joueur2.setHeight(Bomberball.taillecase);
-        joueur2.setPosition(0,Gdx.graphics.getHeight()-8*Bomberball.taillecase);
+        joueur2.setHeight(2*Bomberball.taillecase);
+        joueur2.setPosition(0,Gdx.graphics.getHeight()-9*Bomberball.taillecase);
 
 
         mouvement2 = new Image(new Texture(Gdx.files.internal("Nombre_mouvement.png")));
         mouvement2.setWidth(jeu.map.getX()+2f*Bomberball.taillecase);
         mouvement2.setHeight(Bomberball.taillecase);
-        mouvement2.setPosition(0,Gdx.graphics.getHeight()-9*Bomberball.taillecase);
+        mouvement2.setPosition(0,Gdx.graphics.getHeight()-10*Bomberball.taillecase);
 
         nbmvt2= new Label(""+personnage2.getPm(),skin);
-        nbmvt2.setBounds(3*Bomberball.taillecase+30,Gdx.graphics.getHeight()-8*Bomberball.taillecase-50,nbmvt2.getWidth(),nbmvt2.getHeight()); //Positionnement à la main
+        nbmvt2.setBounds(3*Bomberball.taillecase+30,Gdx.graphics.getHeight()-9*Bomberball.taillecase-50,nbmvt2.getWidth(),nbmvt2.getHeight()); //Positionnement à la main
 
         bombe2=new Image(new Texture(Gdx.files.internal("Nombre_bombe.png")));
         bombe2.setWidth(jeu.map.getX()+2f*Bomberball.taillecase);
         bombe2.setHeight(Bomberball.taillecase);
-        bombe2.setPosition(0,Gdx.graphics.getHeight()- 10*Bomberball.taillecase);
+        bombe2.setPosition(0,Gdx.graphics.getHeight()- 11*Bomberball.taillecase);
 
         nbBombe2= new Label(""+personnage2.getNbBombe(),skin);
-        nbBombe2.setBounds(Bomberball.taillecase*3+30,Gdx.graphics.getHeight()-9*Bomberball.taillecase-60,nbBombe1.getWidth(),nbBombe1.getHeight()); //Positionnement à la main
+        nbBombe2.setBounds(Bomberball.taillecase*3+30,Gdx.graphics.getHeight()-10*Bomberball.taillecase-60,nbBombe1.getWidth(),nbBombe1.getHeight()); //Positionnement à la main
 
         explosion2=new Image(new Texture(Gdx.files.internal("Portée_bombe.png")));
         explosion2.setWidth(jeu.map.getX()+2f*Bomberball.taillecase);
         explosion2.setHeight(Bomberball.taillecase);
-        explosion2.setPosition(0,Gdx.graphics.getHeight()-11*Bomberball.taillecase);
+        explosion2.setPosition(0,Gdx.graphics.getHeight()-12*Bomberball.taillecase);
 
         porteExplo2 = new Label(""+personnage2.getTaille(),skin);
-        porteExplo2.setBounds(Bomberball.taillecase*3+30,Gdx.graphics.getHeight()-10*Bomberball.taillecase-60,porteExplo1.getWidth(),porteExplo1.getHeight()); //Positionnement à la main
+        porteExplo2.setBounds(Bomberball.taillecase*3+30,Gdx.graphics.getHeight()-11*Bomberball.taillecase-60,porteExplo1.getWidth(),porteExplo1.getHeight()); //Positionnement à la main
 
 
         pousse2=new Image(new Texture(Gdx.files.internal("icone_Bonus_pousser.png")));
         pousse2.setWidth(jeu.map.getX()+2f*Bomberball.taillecase);
         pousse2.setHeight(Bomberball.taillecase);
-        pousse2.setPosition(0,Gdx.graphics.getHeight()-12*Bomberball.taillecase);
+        pousse2.setPosition(0,Gdx.graphics.getHeight()-13*Bomberball.taillecase);
 
         player2=new Image(Bomberball.multiTexture[20]);
-        player2.setBounds(3*Bomberball.taillecase+30,Gdx.graphics.getHeight()-7*Bomberball.taillecase-50,Bomberball.taillecase,Bomberball.taillecase);
+        player2.setBounds(3*Bomberball.taillecase+30,Gdx.graphics.getHeight()-8*Bomberball.taillecase-50,Bomberball.taillecase,Bomberball.taillecase);
 
         joueur3=new Image(new Texture(Gdx.files.internal("Panneau_joueur.png")));
         joueur3.setWidth(jeu.map.getX()+2f*Bomberball.taillecase);
-        joueur3.setHeight(Bomberball.taillecase);
-        joueur3.setPosition(jeu.map.getX()+jeu.map.tailleX(),Gdx.graphics.getHeight()-2*Bomberball.taillecase);
+        joueur3.setHeight(2*Bomberball.taillecase);
+        joueur3.setPosition(jeu.map.getX()+17f*Bomberball.taillecase,Gdx.graphics.getHeight()-2*Bomberball.taillecase);
 
 
         mouvement3 = new Image(new Texture(Gdx.files.internal("Nombre_mouvement.png")));
         mouvement3.setWidth(jeu.map.getX()+2f*Bomberball.taillecase);
         mouvement3.setHeight(Bomberball.taillecase);
-        mouvement3.setPosition(jeu.map.getX()+jeu.map.tailleX(),Gdx.graphics.getHeight()-3*Bomberball.taillecase);
+        mouvement3.setPosition(jeu.map.getX()+17f*Bomberball.taillecase,Gdx.graphics.getHeight()-3*Bomberball.taillecase);
 
         nbmvt3= new Label(""+personnage3.getPm(),skin);
-        nbmvt3.setBounds(3*Bomberball.taillecase+30,Gdx.graphics.getHeight()-2*Bomberball.taillecase-50,nbmvt3.getWidth(),nbmvt3.getHeight()); //Positionnement à la main
+        nbmvt3.setBounds(jeu.map.getX()+17f*Bomberball.taillecase+Bomberball.taillecase*3+30,Gdx.graphics.getHeight()-2*Bomberball.taillecase-50,nbmvt3.getWidth(),nbmvt3.getHeight()); //Positionnement à la main
 
         bombe3=new Image(new Texture(Gdx.files.internal("Nombre_bombe.png")));
         bombe3.setWidth(jeu.map.getX()+2f*Bomberball.taillecase);
         bombe3.setHeight(Bomberball.taillecase);
-        bombe3.setPosition(jeu.map.getX()+jeu.map.tailleX(),Gdx.graphics.getHeight()- 4*Bomberball.taillecase);
+        bombe3.setPosition(jeu.map.getX()+17f*Bomberball.taillecase,Gdx.graphics.getHeight()- 4*Bomberball.taillecase);
 
         nbBombe3= new Label(""+personnage3.getNbBombe(),skin);
-        nbBombe3.setBounds(Bomberball.taillecase*3+30,Gdx.graphics.getHeight()-3*Bomberball.taillecase-60,nbBombe3.getWidth(),nbBombe3.getHeight()); //Positionnement à la main
+        nbBombe3.setBounds(jeu.map.getX()+17f*Bomberball.taillecase+Bomberball.taillecase*3+30,Gdx.graphics.getHeight()-3*Bomberball.taillecase-60,nbBombe3.getWidth(),nbBombe3.getHeight()); //Positionnement à la main
 
         explosion3=new Image(new Texture(Gdx.files.internal("Portée_bombe.png")));
         explosion3.setWidth(jeu.map.getX()+2f*Bomberball.taillecase);
         explosion3.setHeight(Bomberball.taillecase);
-        explosion3.setPosition(jeu.map.getX()+jeu.map.tailleX(),Gdx.graphics.getHeight()-5*Bomberball.taillecase);
+        explosion3.setPosition(jeu.map.getX()+17f*Bomberball.taillecase,Gdx.graphics.getHeight()-5*Bomberball.taillecase);
 
         porteExplo3 = new Label(""+personnage3.getTaille(),skin);
-        porteExplo3.setBounds(Bomberball.taillecase*3+30,Gdx.graphics.getHeight()-4*Bomberball.taillecase-60,porteExplo1.getWidth(),porteExplo1.getHeight()); //Positionnement à la main
+        porteExplo3.setBounds(jeu.map.getX()+17f*Bomberball.taillecase+Bomberball.taillecase*3+30,Gdx.graphics.getHeight()-4*Bomberball.taillecase-60,porteExplo1.getWidth(),porteExplo1.getHeight()); //Positionnement à la main
 
 
         pousse3=new Image(new Texture(Gdx.files.internal("icone_Bonus_pousser.png")));
         pousse3.setWidth(jeu.map.getX()+2f*Bomberball.taillecase);
         pousse3.setHeight(Bomberball.taillecase);
-        pousse3.setPosition(jeu.map.getX()+jeu.map.tailleX(),Gdx.graphics.getHeight()-12*Bomberball.taillecase);
+        pousse3.setPosition(jeu.map.getX()+17f*Bomberball.taillecase,Gdx.graphics.getHeight()-6*Bomberball.taillecase);
 
         player3=new Image(Bomberball.multiTexture[21]);
-        player3.setBounds(jeu.map.getX()+jeu.map.tailleX()+3*Bomberball.taillecase+30,Gdx.graphics.getHeight()-7*Bomberball.taillecase-50,Bomberball.taillecase,Bomberball.taillecase);
+        player3.setBounds(jeu.map.getX()+jeu.map.tailleX()+17*Bomberball.taillecase+3*Bomberball.taillecase,Gdx.graphics.getHeight()-Bomberball.taillecase-50,Bomberball.taillecase,Bomberball.taillecase);
 
         joueur4=new Image(new Texture(Gdx.files.internal("Panneau_joueur.png")));
         joueur4.setWidth(jeu.map.getX()+2f*Bomberball.taillecase);
-        joueur4.setHeight(Bomberball.taillecase);
-        joueur4.setPosition(jeu.map.getX()+jeu.map.tailleX(),Gdx.graphics.getHeight()-8*Bomberball.taillecase);
+        joueur4.setHeight(2*Bomberball.taillecase);
+        joueur4.setPosition(jeu.map.getX()+17f*Bomberball.taillecase,Gdx.graphics.getHeight()-9*Bomberball.taillecase);
 
 
         mouvement4 = new Image(new Texture(Gdx.files.internal("Nombre_mouvement.png")));
         mouvement4.setWidth(jeu.map.getX()+2f*Bomberball.taillecase);
         mouvement4.setHeight(Bomberball.taillecase);
-        mouvement4.setPosition(jeu.map.getX()+jeu.map.tailleX(),Gdx.graphics.getHeight()-9*Bomberball.taillecase);
+        mouvement4.setPosition(jeu.map.getX()+17f*Bomberball.taillecase,Gdx.graphics.getHeight()-10*Bomberball.taillecase);
 
         nbmvt4= new Label(""+personnage4.getPm(),skin);
-        nbmvt4.setBounds(3*Bomberball.taillecase+30,Gdx.graphics.getHeight()-8*Bomberball.taillecase-50,nbmvt3.getWidth(),nbmvt3.getHeight()); //Positionnement à la main
+        nbmvt4.setBounds(jeu.map.getX()+17f*Bomberball.taillecase+Bomberball.taillecase*3+30,Gdx.graphics.getHeight()-9*Bomberball.taillecase-50,nbmvt3.getWidth(),nbmvt3.getHeight()); //Positionnement à la main
 
         bombe4=new Image(new Texture(Gdx.files.internal("Nombre_bombe.png")));
         bombe4.setWidth(jeu.map.getX()+2f*Bomberball.taillecase);
         bombe4.setHeight(Bomberball.taillecase);
-        bombe4.setPosition(jeu.map.getX()+jeu.map.tailleX(),Gdx.graphics.getHeight()- 10*Bomberball.taillecase);
+        bombe4.setPosition(jeu.map.getX()+17f*Bomberball.taillecase,Gdx.graphics.getHeight()- 11*Bomberball.taillecase);
 
         nbBombe4= new Label(""+personnage4.getNbBombe(),skin);
-        nbBombe4.setBounds(Bomberball.taillecase*3+30,Gdx.graphics.getHeight()-9*Bomberball.taillecase-60,nbBombe3.getWidth(),nbBombe3.getHeight()); //Positionnement à la main
+        nbBombe4.setBounds(jeu.map.getX()+17f*Bomberball.taillecase+Bomberball.taillecase*3+30,Gdx.graphics.getHeight()-10*Bomberball.taillecase-60,nbBombe3.getWidth(),nbBombe3.getHeight()); //Positionnement à la main
 
         explosion4=new Image(new Texture(Gdx.files.internal("Portée_bombe.png")));
         explosion4.setWidth(jeu.map.getX()+2f*Bomberball.taillecase);
         explosion4.setHeight(Bomberball.taillecase);
-        explosion4.setPosition(jeu.map.getX()+jeu.map.tailleX(),Gdx.graphics.getHeight()-11*Bomberball.taillecase);
+        explosion4.setPosition(jeu.map.getX()+17f*Bomberball.taillecase,Gdx.graphics.getHeight()-12*Bomberball.taillecase);
 
-        porteExplo3 = new Label(""+personnage4.getTaille(),skin);
-        porteExplo3.setBounds(Bomberball.taillecase*3+30,Gdx.graphics.getHeight()-10*Bomberball.taillecase-60,porteExplo1.getWidth(),porteExplo1.getHeight()); //Positionnement à la main
+        porteExplo4 = new Label(""+personnage4.getTaille(),skin);
+        porteExplo4.setBounds(jeu.map.getX()+17f*Bomberball.taillecase+Bomberball.taillecase*3+30,Gdx.graphics.getHeight()-11*Bomberball.taillecase-60,porteExplo1.getWidth(),porteExplo1.getHeight()); //Positionnement à la main
 
 
         pousse4=new Image(new Texture(Gdx.files.internal("icone_Bonus_pousser.png")));
         pousse4.setWidth(jeu.map.getX()+2f*Bomberball.taillecase);
         pousse4.setHeight(Bomberball.taillecase);
-        pousse4.setPosition(jeu.map.getX()+jeu.map.tailleX(),Gdx.graphics.getHeight()-12*Bomberball.taillecase);
+        pousse4.setPosition(jeu.map.getX()+17f*Bomberball.taillecase,Gdx.graphics.getHeight()-13*Bomberball.taillecase);
 
         player4=new Image(Bomberball.multiTexture[22]);
-        player4.setBounds(jeu.map.getX()+jeu.map.tailleX()+3*Bomberball.taillecase+30,Gdx.graphics.getHeight()-7*Bomberball.taillecase-50,Bomberball.taillecase,Bomberball.taillecase);
-
-
+        player4.setBounds(jeu.map.getX()+jeu.map.tailleX()+17*Bomberball.taillecase+3*Bomberball.taillecase,Gdx.graphics.getHeight()-8*Bomberball.taillecase-50,Bomberball.taillecase,Bomberball.taillecase);
 
 
 
@@ -290,8 +289,32 @@ public class Multijoueur extends Etat implements Screen {//etat multijoueur
         jeu.addActor(porteExplo1);
         jeu.addActor(pousse1);
         jeu.addActor(player1);
+        jeu.addActor(joueur2);
+        jeu.addActor(mouvement2);
+        jeu.addActor(nbmvt2);
+        jeu.addActor(bombe2);
+        jeu.addActor(nbBombe2);
+        jeu.addActor(explosion2);
+        jeu.addActor(porteExplo2);
+        jeu.addActor(pousse2);
         jeu.addActor(player2);
+        jeu.addActor(joueur3);
+        jeu.addActor(mouvement3);
+        jeu.addActor(nbmvt3);
+        jeu.addActor(bombe3);
+        jeu.addActor(nbBombe3);
+        jeu.addActor(explosion3);
+        jeu.addActor(porteExplo3);
+        jeu.addActor(pousse3);
         jeu.addActor(player3);
+        jeu.addActor(joueur4);
+        jeu.addActor(mouvement4);
+        jeu.addActor(nbmvt4);
+        jeu.addActor(bombe4);
+        jeu.addActor(nbBombe4);
+        jeu.addActor(explosion4);
+        jeu.addActor(porteExplo4);
+        jeu.addActor(pousse4);
         jeu.addActor(player4);
         jeu.addActor(jeu.map);
 
@@ -309,6 +332,13 @@ public class Multijoueur extends Etat implements Screen {//etat multijoueur
                     if (pm > 0) {
                         b = joueur.deplacerDroite();
                         pm = ((b) ? pm - 1 : pm);
+                        switch(tour){
+                            case 0:     jeu.removeActor(nbmvt1);nbmvt1.setText(""+pm);jeu.addActor(nbmvt1);break();
+                            case 1:     jeu.removeActor(nbmvt2);nbmvt2.setText(""+pm);jeu.addActor(nbmvt2);break();
+                            case 2:     jeu.removeActor(nbmvt3);nbmvt3.setText(""+pm);jeu.addActor(nbmvt3);break();
+                            case 3:     jeu.removeActor(nbmvt4);nbmvt4.setText(""+pm);jeu.addActor(nbmvt4);break();
+                            case default:break();
+                        }
                     }
                 }
 
@@ -316,24 +346,48 @@ public class Multijoueur extends Etat implements Screen {//etat multijoueur
                     if (pm > 0) {
                         b = joueur.deplacerGauche();
                         pm = ((b) ? pm - 1 : pm);
+                        switch(tour){
+                            case 0:     jeu.removeActor(nbmvt1);nbmvt1.setText(""+pm);jeu.addActor(nbmvt1);break();
+                            case 1:     jeu.removeActor(nbmvt2);nbmvt2.setText(""+pm);jeu.addActor(nbmvt2);break();
+                            case 2:     jeu.removeActor(nbmvt3);nbmvt3.setText(""+pm);jeu.addActor(nbmvt3);break();
+                            case 3:     jeu.removeActor(nbmvt4);nbmvt4.setText(""+pm);jeu.addActor(nbmvt4);break();
+                            case default:break();
                     }
                 }
                 if (keycode == Input.Keys.DOWN) {
                     if (pm > 0) {
                         b = joueur.deplacerBas();
                         pm = ((b) ? pm - 1 : pm);
+                        switch(tour){
+                            case 0:     jeu.removeActor(nbmvt1);nbmvt1.setText(""+pm);jeu.addActor(nbmvt1);break();
+                            case 1:     jeu.removeActor(nbmvt2);nbmvt2.setText(""+pm);jeu.addActor(nbmvt2);break();
+                            case 2:     jeu.removeActor(nbmvt3);nbmvt3.setText(""+pm);jeu.addActor(nbmvt3);break();
+                            case 3:     jeu.removeActor(nbmvt4);nbmvt4.setText(""+pm);jeu.addActor(nbmvt4);break();
+                            case default:break();
                     }
                 }
                 if (keycode == Input.Keys.UP) {
                     if (pm > 0) {
                         b = joueur.deplacerHaut();
                         pm = ((b) ? pm - 1 : pm);
+                        switch(tour){
+                            case 0:     jeu.removeActor(nbmvt1);nbmvt1.setText(""+pm);jeu.addActor(nbmvt1);break();
+                            case 1:     jeu.removeActor(nbmvt2);nbmvt2.setText(""+pm);jeu.addActor(nbmvt2);break();
+                            case 2:     jeu.removeActor(nbmvt3);nbmvt3.setText(""+pm);jeu.addActor(nbmvt3);break();
+                            case 3:     jeu.removeActor(nbmvt4);nbmvt4.setText(""+pm);jeu.addActor(nbmvt4);break();
+                            case default:break();
                     }
                 }
                 if (keycode == Input.Keys.SPACE) {
                     if (nb > 0) {
                         b = joueur.poserBombe();
                         nb = ((b) ? nb - 1 : nb);
+                        switch(tour){
+                            case 0:     nbBombe1.setText(""+nb);break();
+                            case 1:     nbBombe2.setText(""+nb);break();
+                            case 2:     nbBombe3.setText(""+nb);jeu.addActor(nbmvt3);break();
+                            case 3:     nbBombe4.setText(""+nb);jeu.addActor(nbmvt4);break();
+                            case default:break();
                     }
                 }
                 if (keycode == Input.Keys.ENTER) {
